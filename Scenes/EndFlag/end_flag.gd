609427b5991f5a42df966054ffd634c70fc82584
CyberUdodPC <43916560+Yadovitiy@users.noreply.github.com>
2026-08-18 21:25:32 +0300

    Добавлено Menu

--- Scenes/EndFlag/end_flag.gd
+++ Scenes/EndFlag/end_flag.gd
@@ -6,5 +6,8 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("Player"):
 		return
+		
+	call_deferred("_load_new_scene")
 
+func _load_new_scene():
 	get_tree().change_scene_to_packed(scene_to_load)
---
+++ Scenes/Menu/menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _on_play_button_pressed() -> void:
+	PlayerStats.score = 0
+	get_tree().change_scene_to_file("res://Scenes/Lvl01/Level01.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
